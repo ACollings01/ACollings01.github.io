5c7a844d05aa5eeb2c4dc82ad79bba15f055a5f6
ACollings01 <alexandercollings@protonmail.com>
2020-06-14 19:22:36 -0400

    Created a bandaid fix for refresh issues, should look into it more later.

--- scripts.js
+++ scripts.js
@@ -12,3 +12,12 @@ function dimButton(sectionButtonID)
     document.getElementById(sectionButtonID).style.backgroundColor = 'rgb(55, 55, 55)';
     document.getElementById(sectionButtonID).parentElement.style.width = '170px';
 }
+
+// Used as a reset since page didn't want to scroll back to ABOUT when page loads
+function startup()
+{
+    // Had to delay this slightly for the page to load, I should find a better way since this might not work for slower connections
+    setTimeout( function() {
+        selectSection("AboutButton");
+    }, 10);
+}
